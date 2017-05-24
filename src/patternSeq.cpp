ab46fab1766642cf03e3df9e71bbe4c3d2d76138
ojpaper <lukasried@googlemail.com>
2017-05-24 23:37:54 +0200

    added files

---
+++ src/patternSeq.cpp
@@ -0,0 +1,60 @@
+#include <omp.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char * argv[]) {
+	int n = atoi(argv[1]);
+	int threads = atoi(argv[2]);
+	cout << "n: " << n << " threads" << threads << endl;
+	int count = 0;
+	short a[n][n];
+	for(int k = 1; k < 100; ++k) {
+		//Initialisierung
+		for(int i = 0; i < n; ++i) {
+			srand (k * (i+1));
+			for(int j = 0; j < n; ++j) {
+				a[i][j] = rand() % 10;
+			}
+		}
+
+		//Pattern-Suche
+		for(int i = 0; i < n; ++i) {
+			for(int j = 0; j < n; ++j) {
+				if(a[i][j] == 1) {
+					if(j+3 < n && a[i][j+1] == 2 && a[i][j+2] == 3 && a[i][j+3] == 4) {
+						count++;
+//						std::cout << "k=" << k << ", horizontal ab a[" <<  i << ", " << j << "]\n";
+//						for(int i = 0; i < n; ++i) {
+//							for(int j = 0; j < n; ++j) {
+//								if(a[i][j] == 1 || a[i][j] == 2 || a[i][j] == 3 || a[i][j] == 4)
+//									std::cout << " " << a[i][j];
+//								else
+//									std::cout << " x";
+//							}
+//							std::cout << "\n";
+//						}
+
+					}
+					if(i+3 < n && a[i+1][j] == 2 && a[i+2][j] == 3 && a[i+3][j] == 4) {
+						count++;
+//						std::cout << "k=" << k << ", vertikal ab a[" <<  i << ", " << j << "]\n";
+//						for(int i = 0; i < n; ++i) {
+//							for(int j = 0; j < n; ++j) {
+//								if(a[i][j] == 1 || a[i][j] == 2 || a[i][j] == 3 || a[i][j] == 4)
+//									std::cout << " " << a[i][j];
+//								else
+//									std::cout << " x";
+//							}
+//							std::cout << "\n";
+//						}
+					}
+				}
+			}
+		}
+	}
+
+	cout << "Anzahl Vorkommen: " << count << "\n";
+	return 0;
+}
