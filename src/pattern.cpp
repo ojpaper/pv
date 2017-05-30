0513c88f2490890c554d4ed219a4c9bcf316308f
ojpaper <lukasried@googlemail.com>
2017-05-31 00:06:18 +0200

    stuff

--- src/pattern.cpp
+++ src/pattern.cpp
@@ -4,15 +4,14 @@
 #include <ctime>
 
 using namespace std;
-
 int main(int argc, char * argv[]) {
 	omp_set_nested(1);
+	omp_set_max_active_levels(2);
 	int n = atoi(argv[1]);
 	int threads = atoi(argv[2]);
 	cout << "n: " << n << " threads " << threads << endl;
-	double count = 0;
+	int count = 0;
 	short a[n][n];
-	//TODO: create place. try stuff to make initialization parallel
 
 	//not this loop
 	for(int k = 1; k < 100; ++k) {
@@ -25,7 +24,7 @@ int main(int argc, char * argv[]) {
 			}
 		}
 		//Pattern-Suche
-		#pragma omp parallel for collapse(2) num_threads(threads) schedule(static) reduction(+:count)
+		#pragma omp parallel for collapse(2) num_threads(threads) schedule(static) reduction(+:count) proc_bind(close)
 		for(int i = 0; i < n; ++i) {
 			for(int j = 0; j < n; ++j) {
 				if(a[i][j] == 1) {
