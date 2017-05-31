f03140ebcf46de5cd6fe3c0f8638b452b2c52f5f
ojpaper <lukasried@googlemail.com>
2017-05-31 23:37:11 +0200

    works properly now

--- src/fill.cpp
+++ src/fill.cpp
@@ -8,73 +8,73 @@ int main(int argc, char * argv[]) {
 	omp_set_max_active_levels(2);
 	int n = atoi(argv[1]);
 	int threads = atoi(argv[2]);
+	omp_set_num_threads(threads);
 	cout << "n: " << n << " threads " << threads << endl;
 
-	double begin = omp_get_wtime();
-
-	long sumB = 0;
+	long sumB;
 	long b[n];
 	bool counter[n];
 	int a[n][3];
 
-	b[0] = 0;
+	sumB = b[0] = 0;
 	counter[0] = 1;
+	a[0][0] = a[0][1] = a[0][2] = 0;
 
 	srand(123);
-	a[0][0] = a[0][1] = a[0][2] = 0;
 	//sequentiell!
 	for(int i = 1; i < n; ++i) {
+		b[i] = 0;
+		counter[i] = 0;
 		a[i][0] = rand() % i;
 		a[i][1] = (rand() % 19) - 9;
 		a[i][2] = (rand() % 3) + 1;
-		cout << a[i][0] << endl;
 	}
 
-	#pragma omp parallel sections num_threads(threads) shared(counter,b)
+	#pragma omp parallel sections
 	{
 		#pragma omp section
 		{
+			#pragma omp parallel for schedule(dynamic) num_threads(threads-1)
 			//Berechnung
-			double start;
-			int help;
-			#pragma omp parallel for schedule(dynamic) private(help) shared(counter,b) num_threads(threads-1)
-			for(int i = 1; i < n; ++i) {
-				int halp = 0;
-				while(halp == 0){
-					//warte
+			for	(int i = 1; i < n; ++i) {
+				int counter_c = 0;
+				while(counter_c == 0){
 					#pragma omp atomic read
-					halp = counter[a[i][0]];
+					counter_c = counter[a[i][0]];
 				}
+				int help;
 				#pragma omp atomic read
 				help = b[a[i][0]];
-
-				start = omp_get_wtime();
+				double start = omp_get_wtime();
 				while(omp_get_wtime() - start < a[i][2]/10) {} //very complicated calculation
+				#pragma omp atomic write
 				b[i] = help + a[i][1];
+				#pragma omp atomic write
+				sumB = sumB + b[i];
 				#pragma omp atomic write
 				counter[i] = 1;
 			}
 		}
+
 		#pragma omp section
 		{
 			//Ausgabe
+			int counter_b;
 			for(int i = 0; i < n; ++i) {
-				int halp = 0;
-				while(halp == 0) {
+				counter_b = 0;
+				while(!counter_b) {
 					#pragma omp atomic read
-					halp = counter[i];
+					counter_b = counter[i];
 				}
 				#pragma omp atomic read
-				halp = b[i];
-				cout << "b[" << i << "] = " << halp<< "\n";
-				sumB += halp;
+				counter_b = b[i];
+				#pragma omp critical
+				{
+					cout << "b[" << i << "] = " << counter_b << "\n";
+				}
 			}
 			cout << "sum = " << sumB << endl;
 		}
 	}
-
-	double end = omp_get_wtime();
-	cout << "time: " << end-begin << endl;
-
 	return 0;
 }
