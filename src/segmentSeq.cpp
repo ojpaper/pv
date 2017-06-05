5040cb5cc837fb883d6d2da31610d97136c07145
Fabian Wurmbach <fabian.wurmbach@gmx.de>
2017-06-05 23:58:40 +0200

    vector --> array

--- src/segmentSeq.cpp
+++ src/segmentSeq.cpp
@@ -4,20 +4,20 @@
 #include <vector>
 using namespace std;
 
-int summe(vector<int> a) {
+/*int summe(vector<int> a) {
 	int sum = 0;
 	for(vector<int>::iterator it=a.begin(); it != a.end(); it++) {
 		sum += *it;
 	}
 	return sum;
-}
+}*/
 
 int main(int argc, char * argv[]) {
 	int n = atoi(argv[1]);
 	int threads = atoi(argv[2]);
 
 	//aufeinanderfolgende zahlen: 94, 14, ...
-	vector<int> a = {
+//	vector<int> a = {
 //			94,
 //			14,
 //			74,
@@ -28,28 +28,34 @@ int main(int argc, char * argv[]) {
 //			23,
 //			27,
 //			2
-	};//n = 10, seed = 123
+//	};//n = 10, seed = 123
+	int a[n];
+	long summe = 0;
 
 	double begin = omp_get_wtime();
 
 	srand(4321);
 	for(int i = 0; i < n; ++i) {
-		a.push_back(rand() % 100 + 1);
+		a[i](rand() % 100 + 1);
+		summe += a[i];
 		//		cout << a[i] << endl;
 	}
 
-	long s = summe(a);
-	vector<long> m;
-	m.push_back(s);
+//	vector<long> m;
 
-	cout << s << endl;
+	long m[n+1];
+	int pushBackCounter = 0;
+	m[pushBackCounter++] = summe;
+	//m.push_back(s);
+
+	cout << summe << endl;
 
 	for(int anzahl = 2; anzahl <= n/2; anzahl++) {
 		int mitte = 0;
-		long links = s/anzahl;
-		long rechts = s;
+		long links = summe/anzahl;
+		long rechts = summe;
 		long segmentCount;
-		long smallest = s;
+		long smallest = summe;
 		long biggest;
 		long segment;
 
@@ -61,16 +67,16 @@ int main(int argc, char * argv[]) {
 			biggest = 0;
 			segmentCount = anzahl-1;
 			for(int i = 0; i < n; i++) {
-				if(segment+a.at(i) <= mitte) {
-					segment += a.at(i);
+				if(segment + a[i] <= mitte) {
+					segment += a[i];
 				} else if(segmentCount > 0) {
 					if(segment > biggest) {
 						biggest = segment;
 					}
-					segment = a.at(i);
+					segment = a[i];
 					segmentCount--;
 				} else {
-					segment += a.at(i);
+					segment += a[i];
 				}
 			}
 			if(segment > biggest) {
@@ -85,19 +91,20 @@ int main(int argc, char * argv[]) {
 				rechts = mitte - 1;
 			}
 		}
-		m.push_back(smallest);
+		m[pushBackCounter++] = smallest;
+		//m.push_back(smallest);
 	}
 
 	/*2, 3, 5, n/4, n/2 - 1, n/2
 	 *
 	 */
 
-	cout << "M(m=2) = " << m.at(1) << endl;
-	cout << "M(m=3) = "  << m.at(2) << endl;
-	cout << "M(m=5) = "  << m.at(4) << endl;
-	cout << "M(m=" << n/4 << ")= "  << m.at(n/4-1) << endl;
-	cout << "M(m=" << n/2-1 << ") = "  << m.at((n/2)-2) << endl;
-	cout << "M(m=" << n/2 << ") = "  << m.at(n/2-1) << endl;
+	cout << "M(m=2) = " << m[1] << endl;
+	cout << "M(m=3) = "  << m[2] << endl;
+	cout << "M(m=5) = "  << m[4] << endl;
+	cout << "M(m=" << n/4 << ")= "  << m[n/4-1] << endl;
+	cout << "M(m=" << n/2-1 << ") = "  << m[((n/2)-2)] << endl;
+	cout << "M(m=" << n/2 << ") = "  << m[(n/2-1)] << endl;
 
 	double end = omp_get_wtime();
 	cout << "time: " << end-begin << endl;
